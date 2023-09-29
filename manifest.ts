7527f9ca9ab7b75d50ee7f4d8d4635de19033671
kyonenya <kyon.musica@gmail.com>
2023-09-29 13:06:39 +0900

    feat: カスタムファンクションのTutorial

--- manifest.ts
+++ manifest.ts
@@ -1,5 +1,7 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
-import { workflow as HelloWorld } from "./hello_world.ts";
+// import { workflow as HelloWorld } from "./hello_world.ts";
+import { def as MySendMessage } from "./my_send_message.ts";
+import { workflow as HelloWorld2 } from "./hello_world_2.ts";
 
 /**
  * The app manifest contains the app's configuration. This
@@ -10,9 +12,9 @@ export default Manifest({
   name: "slack-deno-playground",
   description: "A blank template for building Slack apps with Deno",
   icon: "assets/default_new_app_icon.png",
-  functions: [],
+  functions: [MySendMessage],
   // ここでワークフローを登録する
-  workflows: [HelloWorld],
+  workflows: [HelloWorld2],
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
